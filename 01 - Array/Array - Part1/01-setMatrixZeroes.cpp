ac4c2fab37a1cb9673f11590a027d52db550b142
priyansh70 <priyansh70890@gmail.com>
2023-06-11 16:56:25 +0530

    add Set Matrix Zero Problem

---
+++ 01 - Array/Array - Part1/01-setMatrixZeroes.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+    void setZero(int i, int j, vector<vector<int>> &matrix, int n, int m)
+    {
+        // Set Row Zero
+        for (int index = 0; index < m; index++)
+        {
+            if (matrix[i][index] != 0)
+            {
+                matrix[i][index] = -1;
+            }
+        }
+        // Set Column Zero
+        for (int index = 0; index < n; index++)
+        {
+            if (matrix[index][j] != 0)
+            {
+                matrix[index][j] = -1;
+            }
+        }
+    }
+
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                // Check matrix element is 0 or not
+                if (matrix[i][j] == 0)
+                {
+                    setZero(i, j, matrix, n, m);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (matrix[i][j] == -1)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
