667c9eb861e728dd3c177c9e0df41495f0aef20d
priyansh70 <priyansh70890@gmail.com>
2023-06-23 19:43:43 +0530

    add middle of the linked list solution

---
+++ 05 - Linked List/02-middleOfTheLL.cpp
@@ -0,0 +1,41 @@
+// Time Complexity - O(n)
+// Space complexity - O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+    Node(int data, Node *next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+Node *findMiddle(Node *head)
+{
+    if (!head || !head->next)
+        return head;
+    Node *slow = head;
+    Node *fast = head;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
