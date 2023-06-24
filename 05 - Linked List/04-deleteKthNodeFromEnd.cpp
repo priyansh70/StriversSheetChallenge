36bf4298b81473477719fefe9e3b5d2d7d95fd97
priyansh70 <priyansh70890@gmail.com>
2023-06-24 15:12:09 +0530

    Add Remove N-th node from back of LinkedList

---
+++ 05 - Linked List/04-deleteKthNodeFromEnd.cpp
@@ -0,0 +1,37 @@
+// Time Complexity - O(n)
+// Space complexity - O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//  Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *removeNthFromEnd(ListNode *head, int n)
+{
+    ListNode *dummy = new ListNode();
+    dummy->next = head;
+    ListNode *fast = dummy;
+    ListNode *slow = dummy;
+
+    for (int i = 1; i <= n; i++)
+    {
+        fast = fast->next;
+    }
+
+    while (fast->next)
+    {
+        fast = fast->next;
+        slow = slow->next;
+    }
+
+    slow->next = slow->next->next;
+    return dummy->next;
+}
