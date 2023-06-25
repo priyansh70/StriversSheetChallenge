36a32eff1261eb5c84e5c7dfd48f80f238bbbb66
priyansh70 <priyansh70890@gmail.com>
2023-06-25 18:59:06 +0530

    add detect cycle in linked list solution

---
+++ 06 - Linked List/02-detectLoopInLL.cpp
@@ -0,0 +1,32 @@
+// Time Complexity - O(N)
+// Space complexity - O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//  Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+bool hasCycle(ListNode *head)
+{
+    if (head == NULL || head->next == NULL)
+        return false;
+
+    ListNode *fast = head;
+    ListNode *slow = head;
+
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+            return true;
+    }
+    return false;
+}
