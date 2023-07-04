1ce55d8106f1db9f39ec6ad9c1a41c312801266c
priyansh70 <priyansh70890@gmail.com>
2023-07-04 23:15:49 +0530

    Modify - correct time and space complexity of ss1

--- 09 - Recursion/01-subsetSum-I.cpp
+++ 09 - Recursion/01-subsetSum-I.cpp
@@ -1,5 +1,8 @@
-// Time Complexity - O(2^t * k) where t is the target, k is the average length
-// Space Complexity - O(k*x), k is the average length and x is the no. of combinations
+// Time Complexity -O(2^n)+O(2^n log(2^n)).
+//  Each index has two ways. You can either pick it up or not pick it.
+//  So for n index time complexity for O(2^n) and for sorting it will take (2^n log(2^n)).
+
+// Space Complexity - O(2^n) for storing subset sums, since 2^n subsets can be generated for an array of size n.
 
 #include <bits/stdc++.h>
 using namespace std;
